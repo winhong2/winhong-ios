468e9505af8ad296509513cd9946b64fa14c4cad
winhong2 <huadi@huadideiMac.local>
2014-09-29 15:28:29 +0800

    license管理

---
+++ WinCenter-iPad/WinCenter-iPad/views/models/LicenseWciVO.h
@@ -0,0 +1,18 @@
+//
+//  LicenseWciVO.h
+//  WinCenter-iPad
+//
+//  Created by huadi on 14-9-29.
+//  Copyright (c) 2014年 huadi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LicenseWciVO : NSObject
+@property NSString *custName;
+@property NSString *orgName;
+@property NSString *email;
+@property NSString *phone;
+@property NSString *IcType;
+@property int IcNum;
+@end
---
+++ WinCenter-iPad/WinCenter-iPad/views/models/LicenseWciVO.m
@@ -0,0 +1,13 @@
+//
+//  LicenseWciVO.m
+//  WinCenter-iPad
+//
+//  Created by huadi on 14-9-29.
+//  Copyright (c) 2014年 huadi. All rights reserved.
+//
+
+#import "LicenseWciVO.h"
+
+@implementation LicenseWciVO
+
+@end
