50541462c54e2cbaf5914c60a05782e66346f7e1
179309463 <179309463@qq.com>
2014-10-10 01:47:27 +0800

    workspace

--- Pods/Pods-WinCenter-iPad-environment.h
+++ Pods/Pods-WinCenter-iPad-environment.h
@@ -6,9 +6,9 @@
 // project.
 
 
-// Unirest
-#define COCOAPODS_POD_AVAILABLE_Unirest
-#define COCOAPODS_VERSION_MAJOR_Unirest 1
-#define COCOAPODS_VERSION_MINOR_Unirest 1
-#define COCOAPODS_VERSION_PATCH_Unirest 3
+// NSObject-ObjectMap
+#define COCOAPODS_POD_AVAILABLE_NSObject_ObjectMap
+#define COCOAPODS_VERSION_MAJOR_NSObject_ObjectMap 2
+#define COCOAPODS_VERSION_MINOR_NSObject_ObjectMap 3
+#define COCOAPODS_VERSION_PATCH_NSObject_ObjectMap 1
 
