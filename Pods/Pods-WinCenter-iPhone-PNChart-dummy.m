1425236552b07c3c3382f000d4b0dc47f733f605
179309463 <179309463@qq.com>
2014-10-10 10:46:49 +0800

    pod

---
+++ Pods/Pods-WinCenter-iPhone-PNChart-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WinCenter_iPhone_PNChart : NSObject
+@end
+@implementation PodsDummy_Pods_WinCenter_iPhone_PNChart
+@end
---
+++ Pods/Pods-WinCenter-iPhone-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WinCenter_iPhone : NSObject
+@end
+@implementation PodsDummy_Pods_WinCenter_iPhone
+@end
