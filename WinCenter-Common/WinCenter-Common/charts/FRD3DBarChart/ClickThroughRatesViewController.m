3624230aec4157ee6e140f38f7cac22c1433ecaf
179309463 <179309463@qq.com>
2014-10-15 11:02:43 +0800

    调整了3D报表

--- WinCenter-Common/WinCenter-Common/charts/FRD3DBarChart/ClickThroughRatesViewController.m
+++ WinCenter-Common/WinCenter-Common/charts/FRD3DBarChart/ClickThroughRatesViewController.m
@@ -13,39 +13,24 @@
 @interface ClickThroughRatesViewController ()
 
 @property (nonatomic,strong) Example2 *example;
-@property (nonatomic, strong) FRD3DBarChartViewController *frd3DBarCharVC;
 @end
 
 @implementation ClickThroughRatesViewController
-@synthesize contentView;
-@synthesize chartView;
 @synthesize example = _example;
-@synthesize frd3DBarCharVC = _frd3DBarCharVC;
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    
-    UIStoryboard *storyboard = self.storyboard;
-    self.frd3DBarCharVC = [storyboard instantiateViewControllerWithIdentifier:@"FRD3DBarChart"];
-    
     self.example = [[Example2 alloc] init];
     self.example.dataSet = kExample2DataSetFacebook;
+    self.frd3dBarChartDelegate = self.example;
     
-    [self addChildViewController:self.frd3DBarCharVC];
-    self.frd3DBarCharVC.frd3dBarChartDelegate = self.example;
+    [super viewDidLoad];
     
-    [self.contentView addSubview:self.frd3DBarCharVC.view];
-    
-    [self.frd3DBarCharVC updateChartAnimated:NO animationDuration:0.0 options:0];
-    
-    [self addChildViewController:self.frd3DBarCharVC];
+    [self updateChartAnimated:NO animationDuration:0.0 options:0];
 }
 
 - (void)viewDidUnload
 {
-    [self setChartView:nil];
-    [self setContentView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -54,15 +39,22 @@
 {
 	return YES;
 }
+- (IBAction)segmentAction:(id)sender {
+    if(((UISegmentedControl*)sender).selectedSegmentIndex==0){
+        [self facebookAction:nil];
+    }else{
+        [self twitterAction:nil];
+    }
+}
 
 - (IBAction)facebookAction:(id)sender {
     [self.example setDataSet:kExample2DataSetFacebook];
-    [self.frd3DBarCharVC updateChartAnimated:YES animationDuration:1.0 options:kUpdateChartOptionsDoNotUpdateLegends];
+    [self updateChartAnimated:YES animationDuration:1.0 options:kUpdateChartOptionsDoNotUpdateLegends];
 }
 
 - (IBAction)twitterAction:(id)sender {
     [self.example setDataSet:kExample2DataSetTwitter];
-    [self.frd3DBarCharVC updateChartAnimated:YES animationDuration:1.0 options:kUpdateChartOptionsDoNotUpdateLegends];
+    [self updateChartAnimated:YES animationDuration:1.0 options:kUpdateChartOptionsDoNotUpdateLegends];
 }
 
 @end
